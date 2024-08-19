03ae38d79a37ad450142b1ea6c304304311f097a
akshay-joseph-justin <akshayjosephjustin721@gmail.com>
2024-08-20 00:51:08 +0530

    templates pages

--- Renter/core/urls.py
+++ Renter/core/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     
     path('',views.index),
+    path('building',views.building),
+    path('renter',views.renter),
+    path('pending',views.pending),
 ]
--- Renter/core/views.py
+++ Renter/core/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def building(request):
+    return render(request,'building.html')
+
+def renter(request):
+    return render(request,'renter.html')
+
+def pending(request):
+    return render(request,'pending.html')

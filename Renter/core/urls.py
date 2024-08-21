3855e79fe7824e3a59c176b09417deead7952ab8
akshay-joseph-justin <akshayjosephjustin721@gmail.com>
2024-08-21 23:43:37 +0530

    add building

--- Renter/core/urls.py
+++ Renter/core/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     
     path('',views.index),
-    path('building',views.building),
-    path('renter',views.renter),
-    path('pending',views.pending),
+    path('building',views.building,name='building'),
+    path('renter',views.renter,name='renter'),
+    path('pending',views.pending,name='pending'),
+    path('add-building',views.add_building,name='add-building'),
+    path('add-renter',views.add_renter,name='add-renter'),
 ]
--- Renter/core/views.py
+++ Renter/core/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Building
 
 # Create your views here.
 
+def add_building(request):
+    if request.method == "GET":
+        return render(request, "add-building.html")
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        
+        building = Building(name=name)
+        building.save()
+        
+        return redirect("/")
+
 def index(request):
-    return render(request,'index.html')
+    if request.method == "GET":
+        context = {
+            "building": Building.objects.all()
+        }
+        return render(request, "index.html", context=context)
+
+
+def add_renter(request):
+    return render(request,'add-renter.html')
 
 def building(request):
     return render(request,'building.html')
@@ -13,3 +35,5 @@ def renter(request):
 
 def pending(request):
     return render(request,'pending.html')
+
+
